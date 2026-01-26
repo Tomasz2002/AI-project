bc7446ff9dcd5cf8d508ef2dc4b68ba471098fd6
Tomasz2002 <121009782+Tomasz2002@users.noreply.github.com>
2026-01-26 15:37:51 +0100

    Refactor Footer component: remove navigation and legal links, update copyright notice

--- frontend/src/layout/Footer/Footer.tsx
+++ frontend/src/layout/Footer/Footer.tsx
@@ -14,42 +14,11 @@ const Footer: React.FC = () => {
               Inteligentna platforma do nauki, która przekształca materiały wideo i tekstowe w interaktywne quizy.
             </p>
           </div>
-
-          {/* Linki */}
-          <div className={`col-12 col-md-6 col-lg-3 ${styles.column}`}>
-            <h6 className={styles.columnTitle}>Nawigacja</h6>
-            <ul className={styles.linksList}>
-              <li><a href="#features" className={styles.link}>Funkcje</a></li>
-              <li><a href="#" className={styles.link}>Cennik</a></li>
-              <li><a href="#" className={styles.link}>FAQ</a></li>
-            </ul>
-          </div>
-
-          {/* Prawne */}
-          <div className={`col-12 col-md-6 col-lg-3 ${styles.column}`}>
-            <h6 className={styles.columnTitle}>Informacje</h6>
-            <ul className={styles.linksList}>
-              <li><a href="#" className={styles.link}>Regulamin</a></li>
-              <li><a href="#" className={styles.link}>Polityka Prywatności</a></li>
-              <li><a href="#" className={styles.link}>Kontakt</a></li>
-            </ul>
-          </div>
-
-          {/* Social */}
-          <div className={`col-12 col-md-6 col-lg-3 ${styles.column}`}>
-            <h6 className={styles.columnTitle}>Śledź nas</h6>
-            <div className={styles.socialLinks}>
-              <a href="#" className={styles.socialLink}>Facebook</a>
-              <a href="#" className={styles.socialLink}>Twitter</a>
-              <a href="#" className={styles.socialLink}>LinkedIn</a>
-            </div>
-          </div>
         </div>
-
         <div className={styles.divider}></div>
         <div className={styles.bottomSection}>
           <p className={styles.copyright}>
-            &copy; 2025 QuizTube. Wszystkie prawa zastrzeżone.
+            &copy; 2025 QuizTube.
           </p>
         </div>
       </div>
